f360cb58fe4bcdcd017915058fde57bd277ff87c
roccomoresi <137230323+roccomoresi@users.noreply.github.com>
2025-09-02 19:24:29 -0300

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,19 @@ android {
 
 dependencies {
 
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Hilt (para inyección de dependencias)
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.51")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
