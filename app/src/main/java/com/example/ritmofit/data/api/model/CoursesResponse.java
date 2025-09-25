4d87dd904fa7f9280b2be05b5a5cfe6aa1b3f7e1
Rocco Moresi <137230323+roccomoresi@users.noreply.github.com>
2025-09-25 10:33:42 -0300

    filter for name

--- app/src/main/java/com/example/ritmofit/data/api/model/CoursesResponse.java
+++ app/src/main/java/com/example/ritmofit/data/api/model/CoursesResponse.java
@@ -1,9 +1,11 @@
 package com.example.ritmofit.data.api.model;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class CoursesResponse {
 
+    @SerializedName("courses")
     private List<CourseResponse> courses;
 
     public List<CourseResponse> getCourses() {
--- app/src/main/java/com/example/ritmofit/home/http/CoursesApi.java
+++ app/src/main/java/com/example/ritmofit/home/http/CoursesApi.java
@@ -8,16 +8,16 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface CoursesApi {
-    @GET("/api/courses/search/byName")
-    Call<PageResponse<CoursesResponse>> getAllBy(@Query("name") String name);
-
-
-    @GET("courses/search/byProfessor")
+    @GET("api/courses/search/byProfessor")
     Call<PageResponse<CoursesResponse>> getAllByProfessor(@Query("professor") String professor);
 
-    @GET("courses/search/byDateBetween")
+    @GET("api/courses/search/byName")
+    Call<PageResponse<CoursesResponse>> getAllBy(@Query("name") String name);
+
+    @GET("api/courses/search/byDateBetween")
     Call<PageResponse<CoursesResponse>> getAllByDateBetween(
             @Query("start") String start,
             @Query("end") String end
     );
+
 }
